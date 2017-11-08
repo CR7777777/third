bd0b395216a993695f451fba2eae10b9afe85f5c
CR7777777 <243273393@qq.com>
2017-11-08 11:55:28 +0800

    print()

--- second/src/main/java/com/example/demo/FirstController.java
+++ second/src/main/java/com/example/demo/FirstController.java
@@ -4,6 +4,9 @@ package com.example.demo;
  * Created by ‘ on 2017/11/8.
  */
 public class FirstController {
+    public String print(){
+        return "abc";
+    }
     public int sum(){
         return 1;
     }
