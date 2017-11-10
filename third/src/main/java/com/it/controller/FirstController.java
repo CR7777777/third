ffb1046744e7e43e4bfb2165f3912da6a6dba62e
CR7777777 <243273393@qq.com>
2017-11-10 11:58:57 +0800

    新建一个controller

---
+++ third/src/main/java/com/it/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.it.controller;
+
+/**
+ * Created by ‘ on 2017/11/10.
+ */
+public class FirstController {
+    public static void main(String[] args) {
+
+    }
+
+    public static void firstDemo(){
+
+    }
+}
