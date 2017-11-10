2e28de89dea4895e3c74f4e73a5dbd00e720b31c
CR7777777 <243273393@qq.com>
2017-11-10 12:00:56 +0800

    sum()

--- third/src/main/java/com/it/controller/FirstController.java
+++ third/src/main/java/com/it/controller/FirstController.java
@@ -5,10 +5,14 @@ package com.it.controller;
  */
 public class FirstController {
     public static void main(String[] args) {
-
+        firstDemo();
     }
 
     public static void firstDemo(){
-
+        System.out.println("有志者事竟成，破釜沉舟百二秦川终归楚");
+        System.out.println("苦心人天不负，卧薪尝胆三千越夹可吞吴");
+    }
+    public static int sum(){
+        return 1;
     }
 }
