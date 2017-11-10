994a389d28192ccf4bb5cccac7ac37e34347cd9a
CR7777777 <243273393@qq.com>
2017-11-10 11:48:30 +0800

    第一个demo的第一个study（）方法

---
+++ third/src/main/java/com/it/test/FirstDemo.java
@@ -0,0 +1,14 @@
+package com.it.test;
+
+/**
+ * Created by ‘ on 2017/11/10.
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void study(){
+        System.out.println("good good study,day day up!");
+    }
+}
